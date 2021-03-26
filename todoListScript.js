1b1e52ec273517d330fe8fbbcfccb04fab5653ca
ruchira <deviruchira27@gmail.com>
2021-03-26 23:32:04 +0530

    files

---
+++ todoListScript.js
@@ -0,0 +1,18 @@
+
+
+
+
+$("#addTaskBtn").click(function addTask(){
+    newTask = $("#newTask").val();
+    if(newTask == ""){
+        alert("Enter task details");
+    }
+    else{
+        oldTasks = $("#tasks").html();
+        newTasks = oldTasks + "<br>" + newTask;
+    
+        $("#newTask").val("");
+        $("#tasks").html(newTasks);
+    }
+});
+
